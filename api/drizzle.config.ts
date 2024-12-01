8a1e0e65ed2a96a466fa12195a440d164b96e761
Davidovtch <eriq2077@gmail.com>
2024-12-01 10:56:05 -0300

    Updated drizzle config,moved some config files to root directory

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './src/db/model/schema.ts',
+    out: '../sql',
+    schema: './src/config/model/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
       url: process.env.DATABASE_URL!,
