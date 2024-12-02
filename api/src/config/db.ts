5f501e5fa6ce56ee6b183163cadc077f7fd02a19
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-01 22:15:41 -0300

    Config database for quert

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -6,4 +6,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 const url: any = process.env.DATABASE_URL
 
-export const db = drizzle(url);
+// export const db = drizzle(url);
+
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { Pool, QueryResult } from "pg";
+
+const pool = new Pool({
+    connectionString: url,
+});
+
+pool.on('error', (err, client) => {
+    console.log('Unexpected error on idle client', err)
+    process.exit(-1);
+})
+
+pool.on('connect', () => {
+    console.log('Base de Dados conectado com sucesso!')
+});
+const query = async (text: string, params?: any[]): Promise<QueryResult<any>> => {
+    return pool.query(text, params);
+  };
+
+export default query
