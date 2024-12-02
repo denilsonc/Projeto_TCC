5bc5e28b532c5fcfd04aed53b57a184b0a0353de
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-01 22:12:36 -0300

    Drizer.config

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     out: './drizzle',
-    schema: './src/db/model/schema.ts',
+    schema: './src/config/model/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
       url: process.env.DATABASE_URL!,
