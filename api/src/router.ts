9c7bff1c696df94891d0dcecb6bc8210e0fb399d
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-01 22:14:32 -0300

    Config and create Routers

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express'
 import { FirstController } from './controllers/teste';
 import { Test } from './routes/check-router';
+import Users from './routes/users.routes'
 
 
 const router = Router()
+const usersRoutes = Users
 
 const firstController = new FirstController()
 const test = new Test()
@@ -11,9 +13,14 @@ const test = new Test()
 router.get('/api', (req: Request,res:Response)=>{
     test.api(req,res)
 })
+router.get('/tables', (req: Request,res:Response)=>{
+    test.showTables(req,res)
+})
 //Routes
 router.get("/", (req: Request, res: Response) => {
     firstController.home(req,res)
 });
+//Rotas de usuários 
+router.use('/users/', usersRoutes)
 
-export { router }
+export default router;
