d532ccf26bc01b7e26a06bbdeb8bae101c950000
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-01 22:13:58 -0300

    Change export for export default

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { router } from './router'
+import router from './router'
 
 export class App {
     private server: express.Application = express()
