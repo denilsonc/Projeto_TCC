e00d1dfdc20eb2c6317293b4668f1f5a644158fd
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-01 22:14:56 -0300

    Create router test

--- api/src/routes/check-router.ts
+++ api/src/routes/check-router.ts
@@ -6,6 +6,8 @@
  */
 
 import { Request, Response} from "express";
+import db from "../config/db";
+import { users } from "../config/model/schema";
 
 export class Test {
     public api(req: Request, res: Response) {
@@ -14,6 +16,18 @@ export class Test {
             message: 'Api acessada com sucesso.',
             version: '1.0.0'
         })
+    }
+    public async showTables( req: Request, res: Response){
+        try {
+            const rows = await db("SELECT * FROM users")
+            return res.status(201).send({ rows: rows })
+        } catch (error) {
+            console.error('mostrar tabelas',error)
+            return res. status(500).send({
+                message: 'deu ruim',
+                error: error
+            })
+        }
     }
 }
 
