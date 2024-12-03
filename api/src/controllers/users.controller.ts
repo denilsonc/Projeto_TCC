a3a2e73280a87af20937c41ba49e39a46a3c6f0e
JefersonBLuz <jefersonbrluz@gmail.com>
2024-12-03 17:06:50 -0300

    Controles de usuário.

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,38 +1,37 @@
 /**
- * Arquivo: src/routes/check-router.ts
- * Descrição: Arquivo responsável pela chamada da API na aplicação.
+ * Arquivo: src/controller/users.controller.ts
+ * Descrição: Arquivo responsável pela lógica do CRUD dos usuários.
  * Data: 01/12/2024
  * Autor: Jeferson Braga
  */
 import { Request, Response } from "express"
-import db from "../config/db";
-import { users } from "../config/model/schema";
+import { db, eq, lt, gte, ne, or } from "../config/db";
+import { address, users } from "../config/model/schema";
+import { console } from "inspector";
 
-interface NewUser{
-    name: string;
-    birthday: any;
-    address: any;
-    email: string;
-    cpf: string;
-    cellphone: string;
-    password: string;
-    privileges: string;
-    active: boolean;
-}
-const createUser = async (req: Request, res: Response): Promise<void> => {
-    const { name, birthday, address, email, cpf, cellphone, password, privileges, active }: NewUser  = req.body;
+// * Método responsavél por criar um novo usuário:
+const createUser = async (req: Request, res: Response) => {
+    const { nameuser, birthday, address, email, cpf, cellphone, password, privileges, active } = req.body;
     try {
-        const { rows } = await db(
-            "INSERT INTO users (name, birthday, email, cpf,cellphone, password, privileges, active, address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-            [name,birthday, email, cpf, cellphone, password, privileges, active, null]
-        );
+        const rows = await db.insert(users).values({
+            name: nameuser,
+            active: true,
+            address: address,
+            birthday: birthday,
+            cellphone: cellphone,
+            cpf: cpf,
+            email: email,
+            password: password,
+            privileges: privileges
+        });
         res.status(201).send({
             message: 'User added successfully!',
             body: {
-                user: {name, birthday, address, email, cpf, cellphone, password, privileges, active}
+                user: {
+                    nameuser, birthday, address, email, cpf, cellphone, password, privileges, active
+                }
             }
         })
-
     } catch (error) {
         console.error('users-view: ', error);
         res.status(500).send({
@@ -41,17 +40,100 @@ const createUser = async (req: Request, res: Response): Promise<void> => {
         })
     }
 }
-
-const viewUsers = async (req: Request, res: Response) => {
+// * Método responsavél listar todos usuários:
+const viewUsersAll = async (req: Request, res: Response) => {
     try {
-        const rows = await db("SELECT * FROM users");
+        const rows = await db.select().from(users);
         res.status(200).send(rows)
     } catch (error) {
-        console.error('users-view: ', error);
+        console.error('viewUsersAll: ', error);
         res.status(500).send({
-            message: 'Ocorreu um erro.'
+            message: 'Ocorreu um erro.',
+            error: error
+        })
+    }
+}
+// * Método responsavél listar usuário especifico:
+const viewUser = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const rows = await db.select().from(users).where(eq(users.id, Number(id))
+        );
+        res.status(200).send(rows)
+    } catch (error) {
+        console.log('viewUser: ', error);
+        res.status(500).send({
+            message: 'Ocorreu um erro.',
+            error: error
+        })
+    }
+}
+// * Método responsavél listar todos os usuário e seus endereços:
+const viewUsersAllWhitAddress = async (req: Request, res: Response) => {
+    try {
+        const rows = await db.select().from(users).innerJoin(address, eq(users.address, address.id))
+        res.status(200).send(rows)
+    } catch (error) {
+        console.log('viewUsersAllWhitAddress: ', error);
+        res.status(500).send({
+            message: 'Ocorreu um erro.',
+            error: error
+        })
+    }
+}
+// * Método responsavél listar usuário específico e seu endereço:
+const viewUsersWhitAddress = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const rows = await db.select().from(users).where(eq(users.id, Number(id))).innerJoin(address, eq(users.address, address.id))
+        res.status(200).send(rows)
+    } catch (error) {
+        console.log('viewUsersWhitAddress: ', error);
+        res.status(500).send({
+            message: 'Ocorreu um erro.',
+            error: error
+        })
+    }
+}
+// * Método responsavél por atualizar dados do usuário:
+const updateUser = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { nameuser, birthday, address, email, cpf, cellphone, password, privileges, active } = req.body;
+    try {
+        const rows = await db.update(users).set({
+            name: nameuser,
+            active: true,
+            address: address,
+            birthday: birthday,
+            cellphone: cellphone,
+            cpf: cpf,
+            email: email,
+            password: password,
+            privileges: privileges
+        }).where(eq(users.id, Number(id)))
+        res.status(200).send({ message: 'Usuário atualizado com sucesso!' })
+    } catch (error) {
+        console.log('updateUser: ', error);
+        res.status(500).send({
+            message: 'Ocorreu um erro.',
+            error: error
+        })
+    }
+}
+// * Método responsável por deletar um determinado usuário por Id:
+const deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const rows = await db.delete(users).where(eq(users.id, Number(id)));
+        res.status(200).send({ message: 'Usuário excluído com sucesso!' })
+    } catch (error) {
+        console.log('updateUser: ', error);
+        res.status(500).send({
+            message: 'Ocorreu um erro.',
+            error: error
         })
     }
 }
 
-export default { viewUsers, createUser }
+
+export default { viewUsersAll, createUser, viewUser, viewUsersAllWhitAddress, viewUsersWhitAddress, updateUser, deleteUser }
