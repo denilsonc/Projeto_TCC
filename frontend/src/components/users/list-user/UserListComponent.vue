7667710ef8019952fa4f791e290fdb9ed84ee5f0
JefersonBLuz <jefersonbrluz@gamil.com>
2024-12-06 00:13:42 -0300

    Criação de página de listar usuários.

---
+++ frontend/src/components/users/list-user/UserListComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> Página de listar Usuários</h1>
+</template>
+<script lang="ts">
+export default {
+    name: 'UserListComponent',
+    data() {
+        return {
+
+        };
+    },
+}
+</script>
